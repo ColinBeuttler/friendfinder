ecb700be8783029f9c07b9b847f42b1462f438b3
Colin <beutt102@mail.chapman.edu>
2019-01-09 10:13:02 -0800

    update

--- logic.js
+++ logic.js
@@ -1,39 +1,15 @@
-var json = {
-    questions: [
-        {
-            name: "name",
-            type: "text",
-            title: "Please enter your name:",
-            placeHolder: "Jon Snow",
-            isRequired: true
-        }, {
-            name: "birthdate",
-            type: "text",
-            inputType: "date",
-            title: "Your birthdate:",
-            isRequired: true
-        }, {
-            name: "color",
-            type: "text",
-            inputType: "color",
-            title: "Your favorite color:"
-        }, {
-            name: "email",
-            type: "text",
-            inputType: "email",
-            title: "Your e-mail:",
-            placeHolder: "jon.snow@nightwatch.org",
-            isRequired: true,
-            validators: [
-                {
-                    type: "email"
-                }
-            ]
-        }
-    ]
-};
+var express = require("express");
+var app = express();
+var bodyParser = require("bodyparser")
 
-window.survey = new Survey.Model(json);
+var PORT = process.env.PORT || 3636
+
+var appParser = bodyParser.json()
+
+app.listen(PORT, function(){
+    console.log("Listening on port" + PORT)
+});
+
 
 survey
     .onComplete
